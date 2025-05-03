d5df491a2eb375e1e0f5b7c6eaa5d692d1e9c847
Dilshan <it21180934@my.sliit.lk>
2025-05-03 19:20:12 +0530

    env implementation ✏️

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ import userRouter from './routes/userRoute.js';
 import jwt from 'jsonwebtoken';
 import orderRouter from './routes/orderRoute.js';
 import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
 const app = express();
 app.use(cors())
 app.use(bodyParser.json())
@@ -36,7 +38,7 @@ app.use(
     }
 )
 
-mongoose.connect("mongodb+srv://admin:123@cluster0.t3vfw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+mongoose.connect(process.env.MONGODB_URL)
 .then(()=>{
     console.log("Connected to the database")
 }).catch(()=>{
